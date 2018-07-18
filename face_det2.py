24d9f98c41d407a1f0d54a4816a0486e54f08ab0
Kübra Çetinkaya <35381438+kubracetinkaya@users.noreply.github.com>
2018-07-18 14:15:17 +0300

    Update face_det2.py

--- face_det2.py
+++ face_det2.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade=cv2.CascadeClassifier('haarcascade-frontalface-default.xml')
+eye_cascade=cv2.CascadeClassifier('haarcascade-eye.xml')
 
 cap=cv2.VideoCapture(1)
 
